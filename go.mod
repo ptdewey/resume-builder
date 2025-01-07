75138e8fe4775ccb3bb5e9eecc2a78a982203b8c
ptdewey <patrickdewey2002@gmail.com>
2025-01-07 15:31:14 -0500

    Added README

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module resume
+module resume-builder
 
 go 1.23.3
 
@@ -6,4 +6,4 @@ require github.com/BurntSushi/toml v1.4.0
 
 require github.com/yuin/gopher-lua v1.1.1
 
-require golang.org/x/text v0.21.0 // indirect
+require golang.org/x/text v0.21.0
--- main.go
+++ main.go
@@ -2,25 +2,40 @@ package main
 
 import (
 	"fmt"
-	"resume/internal"
+	"os"
+	"os/exec"
+	"resume-builder/internal"
 	"strings"
 )
 
 func main() {
-	// TODO: take in input file as cli arg
+	var contentsPath string
+	if len(os.Args) > 1 {
+		contentsPath = os.Args[1]
+	} else {
+		contentsPath = "./examples/contents.lua"
+	}
 
-	// contentsPath := "./contents.lua"
-	contentsPath := "./templates/template.lua"
+	fmt.Println("Parsing resume contents...")
 	contents, err := internal.ParseLuaResumeContents(contentsPath)
 	if err != nil {
 		panic(err)
 	}
 
-	templatePath := "./template.typ"
+	fmt.Println("Populating template file...")
+	templatePath := "./templates/template.typ"
 	outputPath := strings.ReplaceAll(contents.Personal.Name, " ", "") + "-Resume.typ"
 	if err := internal.PopulateTemplate(contents, templatePath, outputPath); err != nil {
 		panic(err)
 	}
 
-	fmt.Println(contents)
+	fmt.Println("Compiling with Typst...")
+	cmd := exec.Command("typst", "compile", outputPath)
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+
+	// TODO: cleanup?
+
+	fmt.Println("Done!")
 }
